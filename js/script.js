cd22872d5c32777e55da65342c06653ec84aaaf5
Roger Saquilayan <rogersaqui23@gmail.com>
2022-08-03 18:44:51 +0800

    fix search-form undefine in js

--- js/script.js
+++ js/script.js
@@ -15,8 +15,6 @@ document.querySelector('#close-contact-info').onclick = () =>{
 
 window.onscroll = () =>{
    navbar.classList.remove('active');
-   searchForm.classList.remove('active');
-   loginForm.classList.remove('active');
    contactInfo.classList.remove('active');
 }
 
